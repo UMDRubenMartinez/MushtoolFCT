8caca49b07c9a5e207c93e2f05c2cbe95ec50887
UMDRubenMartinez <martinez.bueno.ruben@alumnat.copernic.cat>
2024-07-04 15:18:14 +0200

    Actualizado:Sistema multilenguaje

--- app/src/main/java/com/example/mushtool_fct/Screen/SettingsScreen.kt
+++ app/src/main/java/com/example/mushtool_fct/Screen/SettingsScreen.kt
@@ -1,9 +1,6 @@
 package com.example.mushtool_fct.Screen
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
-import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -36,6 +33,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.mushtool_fct.Entity.MultiLanguage
 import com.example.mushtool_fct.Screen.Auth.logOut
 import java.util.Locale
 
@@ -45,6 +43,7 @@ fun SettingsScreen(navController: NavHostController) {
     var context = LocalContext.current
     var expanded by remember { mutableStateOf(false) }
     var selectedLanguage by remember { mutableStateOf("Español") }
+    val multiLanguage = MultiLanguage()
 
     val languageCodes = mapOf(
         "Español" to "es",
@@ -90,7 +89,7 @@ fun SettingsScreen(navController: NavHostController) {
                         DropdownMenuItem(onClick = {
                             selectedLanguage = language
                             expanded = false
-                            cambiarIdioma(
+                            multiLanguage.cambiarIdioma(
                                 code,
                                 context
                             ) // Aquí se llama al método cambiarIdioma con el código ISO del idioma seleccionado
@@ -116,46 +115,3 @@ fun SettingsScreen(navController: NavHostController) {
 }
 
 
-fun cambiarIdioma(idioma: String, context: Context) {
-    IdiomaManager.actualizarIdioma(idioma, context)
-    recreateActivity(context)
-}
-
-fun recreateActivity(context: Context) {
-    val intent = context.packageManager.getLaunchIntentForPackage(context.packageName)
-    intent?.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-    context.startActivity(intent)
-}
-
-object IdiomaManager {
-    private const val PREFERENCIAS_IDIOMA = "idioma_pref"
-    private const val IDIOMA_KEY = "idioma"
-
-    fun actualizarIdioma(idioma: String, context: Context) {
-        val locale = Locale(idioma)
-        Locale.setDefault(locale)
-
-        val resources = context.resources
-        val configuration = Configuration(resources.configuration)
-        configuration.setLocale(locale)
-
-        context.createConfigurationContext(configuration)
-        resources.updateConfiguration(configuration, resources.displayMetrics)
-
-        // Guardar el idioma seleccionado en SharedPreferences
-        guardarIdiomaSeleccionado(idioma, context)
-    }
-
-    fun cargarIdiomaGuardado(context: Context): String {
-        val sharedPreferences =
-            context.getSharedPreferences(PREFERENCIAS_IDIOMA, Context.MODE_PRIVATE)
-        return sharedPreferences.getString(IDIOMA_KEY, Locale.getDefault().language)
-            ?: Locale.getDefault().language
-    }
-
-    private fun guardarIdiomaSeleccionado(idioma: String, context: Context) {
-        val sharedPreferences =
-            context.getSharedPreferences(PREFERENCIAS_IDIOMA, Context.MODE_PRIVATE)
-        sharedPreferences.edit().putString(IDIOMA_KEY, idioma).apply()
-    }
-}
